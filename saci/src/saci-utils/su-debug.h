29548f28fddd49cbf96ad42bc889af607ec56b3e
RyanQueirozS <ryanqueiroz@proton.me>
2025-05-03 17:32:15 -0300

    refactor: changed filetree refactor: remade styling feature: added clang-tidy

--- saci/src/saci-utils/su-debug.h
+++ saci/src/saci-utils/su-debug.h
@@ -1,3 +1,4 @@
+// TODO doc
 /**
  * @file su-debug.h
  * @brief This header defines common debug functions and constants used in the saciCORE
@@ -115,21 +116,6 @@ void sa_Log_Debug(enum sa_logDebugType type, enum sa_logContext context,
                   const char* message, const char* file, int line);
 
 /**
- * @fn sa_Log_Warn
- * @brief Logs a warning using saci's print template
- * @note DO NOT USE THIS. There is already a provided macro that reduces param
- * amount
- *
- * @param[in] context The context of the warn
- * @param[in] message The message explaining the warn
- * @param[in] file The file the warn comes from
- * @param[in] line The line the warn comes from
- */
-void sa_Log_Warn(enum sa_logSeverity severity,
-                 enum sa_logContext context,
-                 const char* message, const char* file, int line);
-
-/**
  * @define sa_Log_Info_Print_m
  * @brief A macro that prints info, using @ref sa_Log_Info
  *
@@ -139,6 +125,13 @@ void sa_Log_Warn(enum sa_logSeverity severity,
 #define sa_Log_Info_Print_m(context, message)              \
     do {                                                   \
         sa_Log_Info(context, message, __FILE__, __LINE__); \
+    } while (0)
+
+#define sa_Log_InfoF_Print_m(context, fmt, ...)                                   \
+    do {                                                                          \
+        char _sa_log_info_buf[2048];                                              \
+        snprintf(_sa_log_info_buf, sizeof(_sa_log_info_buf), fmt, ##__VA_ARGS__); \
+        sa_Log_Info_Print_m(context, _sa_log_info_buf);                           \
     } while (0)
 
 /**
@@ -166,15 +159,53 @@ void sa_Log_Warn(enum sa_logSeverity severity,
 #endif
 
 /**
- * @define sa_Log_Debug_Print_m
- * @brief A macro that prints info, using @ref sa_Log_Info
+ * @define sa_Log_DebugF_Print_m
+ * @brief A macro that prints formatted debug info, using @ref sa_Log_Debug
  *
+ * @param[in] type The type of the info
  * @param[in] context The context of the info
  * @param[in] message The message explaining the info
+ */
+#if defined(SACI_DEBUG_MODE_ALL) || defined(SACI_DEBUG_MODE_WINDOWING) ||   \
+    defined(SACI_DEBUG_MODE_TEXTURE) || defined(SACI_DEBUG_MODE_MODEL) ||   \
+    defined(SACI_DEBUG_MODE_OPENGL) || defined(SACI_DEBUG_MODE_RENDERER) || \
+    defined(SACI_DEBUG_MODE_RENDERER_FUNCTIONS) ||                          \
+    defined(SACI_DEBUG_MODE_RENDERER_BATCH) ||                              \
+    defined(SACI_DEBUG_MODE_RENDERER_CALL)
+#  define sa_Log_DebugF_Print_m(debug_type, context, fmt, ...)                    \
+      do {                                                                        \
+          char _sa_log_dbg_buf[2048];                                             \
+          snprintf(_sa_log_dbg_buf, sizeof(_sa_log_dbg_buf), fmt, ##__VA_ARGS__); \
+          sa_Log_Debug_Print_m(debug_type, context, _sa_log_dbg_buf);             \
+      } while (0)
+#else
+#  define sa_Log_DebugF_Print_m(debug_type, context, message)
+#endif
+
+/**
+ * @definesa_Log_Warn_Print_m(
+ * @brief A macro that prints warnings, using @ref sa_Log_Info
+ *
+ * @param[in] context The context of the warning
+ * @param[in] message The message explaining the warning
  */
 #define sa_Log_Warn_Print_m(severity, context, message)              \
     do {                                                             \
         sa_Log_Warn(severity, context, message, __FILE__, __LINE__); \
+    } while (0)
+
+/**
+ * @definesa_Log_WarnF_Print_m
+ * @brief A macro that prints formatted warnings, using @ref sa_Log_Info
+ *
+ * @param[in] context The context of the warning
+ * @param[in] message The message explaining the warning
+ */
+#define sa_Log_WarnF_Print_m(severity, context, fmt, ...)                         \
+    do {                                                                          \
+        char _sa_log_warn_buf[2048];                                              \
+        snprintf(_sa_log_warn_buf, sizeof(_sa_log_warn_buf), fmt, ##__VA_ARGS__); \
+        sa_Log_Warn_Print_m(severity, context, _sa_log_warn_buf);                 \
     } while (0)
 
 /**
@@ -190,6 +221,20 @@ void sa_Log_Warn(enum sa_logSeverity severity,
     } while (0)
 
 /**
+ * @definesa_Log_ErrorF_Print_m
+ * @brief A macro that prints formatted errors, using @ref sa_Log_Error
+ *
+ * @param[in] context The context of the error
+ * @param[in] message The message explaining the error
+ */
+#define sa_Log_ErrorF_Print_m(severity, context, fmt, ...)                      \
+    do {                                                                        \
+        char _sa_log_err_buf[2048];                                             \
+        snprintf(_sa_log_err_buf, sizeof(_sa_log_err_buf), fmt, ##__VA_ARGS__); \
+        sa_Log_Error_Print_m(severity, context, _sa_log_err_buf);               \
+    } while (0)
+
+/**
  * @define sa_Log_Assert_Message_m
  * @brief A macro that asserts conditions
  *
@@ -202,6 +247,17 @@ void sa_Log_Warn(enum sa_logSeverity severity,
             fprintf(stderr, "Assertion failed at %s:%d\nREASON: %s\n", __func__, __LINE__, message); \
             exit(EXIT_FAILURE);                                                                      \
         }                                                                                            \
+    } while (0)
+
+#define sa_Log_AssertF_Message_m(condition, fmt, ...)                                     \
+    do {                                                                                  \
+        if (!(condition)) {                                                               \
+            char _sa_log_assert_buf[1024];                                                \
+            snprintf(_sa_log_assert_buf, sizeof(_sa_log_assert_buf), fmt, ##__VA_ARGS__); \
+            fprintf(stderr, "Assertion failed at %s:%d\nREASON: %s\n",                    \
+                    __func__, __LINE__, _sa_log_assert_buf);                              \
+            exit(EXIT_FAILURE);                                                           \
+        }                                                                                 \
     } while (0)
 
 // TODO doc
