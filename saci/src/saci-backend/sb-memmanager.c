f41adcba65bc322d81c4034ad3e6eb060acefe19
RyanQueirozS <ryanqueiroz@proton.me>
2025-09-30 19:13:32 -0300

    refactor: made functions smalled and easier to debug

--- saci/src/saci-backend/sb-memmanager.c
+++ saci/src/saci-backend/sb-memmanager.c
@@ -4,14 +4,23 @@
 #include "saci-utils/su-general.h"
 
 #include <arena/arena.h>
+#include <saci-utils/su-types.h>
 #include <stdio.h>
+
+/* === Internal === */
+
+su_Bool sb__mem_alloc_arena(void* pool, const su_U64 capacity, const su_U64 size, void** mem_out);
+
+su_Bool sb__mem_malloc(su_U64* capacity_out, const su_U64 size, void** mem_out);
+
+/* === Header impl === */
 
 struct {
     su_U64 size_now;
     su_U64 capacity;
 
-    void* arena; // Will either be a continuously reallocated chunk or an
-                 // arena depending on build type
+    void* pool; // Will either be a continuously reallocated chunk or an
+                // pool depending on build type
 } sb__mem_manager[sb_MEM_CONTEXT_COUNT] = {0};
 
 struct {
@@ -24,7 +33,10 @@ struct {
 
 void sb_mem_init(const su_Bool use_arenas) {
     if (sb__mem_manager_cfg.is_initialized) {
-        su_LOG_ERROR_M(su_LOG_TYPE_DEV, su_LOG_ERROR_SEVERITY_CRASH, su_LOG_CONTEXT_CORE_MEMORY_MANAGER, "Trying to reinitialize memory allocator");
+        su_LOG_ERROR_M(
+            su_LOG_TYPE_DEV, su_LOG_ERROR_SEVERITY_CRASH,
+            su_LOG_CONTEXT_CORE_MEMORY_MANAGER,
+            "Trying to reinitialize memory allocator");
         return;
     }
     sb__mem_manager_cfg.is_arena_based = use_arenas;
@@ -35,30 +47,21 @@ su_Bool sb_mem_alloc(const enum sb_MemContext ctx,
                      const su_U64 element_size,
                      void** mem_out) {
     if (element_size != 0 && count > UINT64_MAX / element_size) {
-        su_LOG_ERRORF_M(su_LOG_TYPE_USER, su_LOG_ERROR_SEVERITY_CRASH, su_LOG_CONTEXT_CORE_MEMORY_MANAGER,
-                        "Overflow in allocation size");
+        su_LOG_ERRORF_M(
+            su_LOG_TYPE_USER,
+            su_LOG_ERROR_SEVERITY_CRASH,
+            su_LOG_CONTEXT_CORE_MEMORY_MANAGER,
+            "Overflow in allocation size");
     }
     su_U64 total_size = count * element_size;
     if (sb__mem_manager_cfg.is_arena_based) {
-        if (sb__mem_manager[ctx].capacity < total_size) {
-            su_LOG_ERRORF_M(
-                su_LOG_TYPE_USER,
-                su_LOG_ERROR_SEVERITY_CRASH,
-                su_LOG_CONTEXT_CORE_MEMORY_MANAGER,
-                "Could not allocate memory: capacity is %lu and asking for %lu",
-                sb__mem_manager[ctx].capacity, total_size);
-        }
-        *mem_out = ArenaPush(sb__mem_manager[ctx].arena, total_size);
-        return su_TRUE;
+        return sb__mem_alloc_arena(
+            sb__mem_manager[ctx].pool,
+            sb__mem_manager[ctx].capacity,
+            total_size, mem_out);
     }
-    *mem_out = su_MALLOC_M(total_size);
-    su_LOG_INFOF_M(su_LOG_TYPE_USER, su_LOG_CONTEXT_CORE_MEMORY_MANAGER, "Allocating %lu bytes", total_size);
-    if (!*mem_out) {
-        return su_FALSE;
-    }
-    sb__mem_manager[ctx].capacity += total_size;
 
-    return su_TRUE;
+    return sb__mem_malloc(&sb__mem_manager[ctx].capacity, total_size, mem_out);
 }
 
 void sb_mem_print_info(void) {
@@ -73,6 +76,40 @@ void sb_mem_print_info(void) {
     }
 
     printf("Allocated %lu bytes of memory. Could not calculate memory used: Not using "
-           "arena based allocator",
+           "pool based allocator",
            capacity_total);
+}
+
+/* === Internal Implementation === */
+
+// This function expects pool and mem_out to be initialized,
+// it is not it's responsability to check it.
+su_Bool sb__mem_alloc_arena(void* pool, const su_U64 capacity, const su_U64 size, void** mem_out) {
+    if (capacity < size) {
+        su_LOG_ERRORF_M(
+            su_LOG_TYPE_USER,
+            su_LOG_ERROR_SEVERITY_CRASH,
+            su_LOG_CONTEXT_CORE_MEMORY_MANAGER,
+            "Could not allocate memory: capacity is %lu and asking for %lu",
+            capacity, size);
+        return su_FALSE;
+    }
+    *mem_out = ArenaPush(pool, size);
+    return (*mem_out) ? su_TRUE : su_FALSE;
+}
+
+su_Bool sb__mem_malloc(su_U64* capacity_out, const su_U64 size, void** mem_out) {
+    if (!size) {
+        su_LOG_ERROR_M(
+            su_LOG_TYPE_USER,
+            su_LOG_ERROR_SEVERITY_HIGH,
+            su_LOG_CONTEXT_CORE_MEMORY_MANAGER,
+            "Could not allocate memory: asking for 0 bytes");
+    }
+    *mem_out = su_MALLOC_M(size);
+    if (!*mem_out) {
+        return su_FALSE;
+    }
+    *capacity_out += size;
+    return su_TRUE;
 }
