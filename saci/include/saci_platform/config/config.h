02e6e724e1044b324ce40f83ebdbdd7d84061730
RyanQueirozS <ryanqueiroz@proton.me>
2026-01-01 19:54:59 -0300

    refactor: changed config.h's organization

--- saci/include/saci_platform/config/config.h
+++ saci/include/saci_platform/config/config.h
@@ -6,6 +6,46 @@
 
 #include <lua5.4/lua.h>
 
+/* === Lua CFG === */
+
+struct PSaciLuaValue {
+    SaciDataType type;
+    union {
+        double number;
+        int boolean;
+        char* string;   /* Caller must free */
+        void* userdata; /* Lua-owned */
+    } data;
+};
+
+typedef lua_State PSaciLuaState;
+
+typedef void (*PSaciLuaArrayIter)(PSaciLuaState* lua, SaciU64 idx, void* user_data);
+
+SACI_API PSaciLuaState* psaci_lua_load(const char* file_path);
+
+SACI_API void psaci_lua_close(PSaciLuaState* state);
+
+SACI_API void psaci_lua_dump_stack(PSaciLuaState* l);
+
+SACI_API void psaci_lua_clear_stack(PSaciLuaState* lua);
+
+SACI_API void psaci_lua_pop(PSaciLuaState* lua, int pops);
+
+SACI_API SaciBool psaci_lua_push_to_stack(PSaciLuaState* lua, const char* path_to_value);
+
+SACI_API SaciBool psaci_lua_push_array_entry_to_stack(PSaciLuaState* lua, const SaciU64 idx);
+
+SACI_API SaciBool psaci_lua_array_iter(PSaciLuaState* lua, const char* path_to_array, PSaciLuaArrayIter array_iter, void* user_data);
+
+// Already pops the data for the "path_to_value"
+SACI_API SaciBool psaci_lua_get_value(PSaciLuaState* lua, const char* path_to_value, struct PSaciLuaValue* value_out, const SaciDataType expected_type);
+
+SACI_API SaciU64 psaci_lua_get_array_length_in_stack(PSaciLuaState* lua);
+
+/* === Renderer CFG === */
+
+// TODO evaluate if these should be exposed
 struct PSaciRendererCfgUniform {
     char* name; // Debug
     SaciDataType type;
@@ -124,41 +164,6 @@ struct PSaciRendererConfig {
     struct PSaciRendererCfgDraw draw;
     struct PSaciRendererCfgPipeline pipeline;
 };
-
-struct PSaciLuaValue {
-    SaciDataType type;
-    union {
-        double number;
-        int boolean;
-        char* string;   /* Caller must free */
-        void* userdata; /* Lua-owned */
-    } data;
-};
-
-typedef lua_State PSaciLuaState;
-
-typedef void (*PSaciLuaArrayIter)(PSaciLuaState* lua, SaciU64 idx, void* user_data);
-
-SACI_API PSaciLuaState* psaci_lua_load(const char* file_path);
-
-SACI_API void psaci_lua_close(PSaciLuaState* state);
-
-SACI_API void psaci_lua_dump_stack(PSaciLuaState* l);
-
-SACI_API void psaci_lua_clear_stack(PSaciLuaState* lua);
-
-SACI_API void psaci_lua_pop(PSaciLuaState* lua, int pops);
-
-SACI_API SaciBool psaci_lua_push_to_stack(PSaciLuaState* lua, const char* path_to_value);
-
-SACI_API SaciBool psaci_lua_push_array_entry_to_stack(PSaciLuaState* lua, const SaciU64 idx);
-
-SACI_API SaciBool psaci_lua_array_iter(PSaciLuaState* lua, const char* path_to_array, PSaciLuaArrayIter array_iter, void* user_data);
-
-// Already pops the data for the "path_to_value"
-SACI_API SaciBool psaci_lua_get_value(PSaciLuaState* lua, const char* path_to_value, struct PSaciLuaValue* value_out, const SaciDataType expected_type);
-
-SACI_API SaciU64 psaci_lua_get_array_length_in_stack(PSaciLuaState* lua);
 
 SACI_API void psaci_cfg_get_renderer(const char* name, struct PSaciRendererConfig* cfg_out, const char* cfg_file_path);
 
