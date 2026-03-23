d844b66d3c2f60df117cc479fb5b509731b842f2
RyanQueirozS <ryanqueiroz@proton.me>
2026-03-23 18:41:59 -0300

    refactor: deleted unused files

--- saci/include/saci_platform/config/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef SACI_PLATFORM_CONFIG_CONFIG_H
-#define SACI_PLATFORM_CONFIG_CONFIG_H
-
-#include "saci_util/memory.h"
-#include "saci_util/types.h"
-
-#endif // SACI_PLATFORM_CONFIG_CONFIG_H
--- saci/src/saci_platform/config/internal/lua_saci_api.h
+++
@@ -1,8 +0,0 @@
-#ifndef SACI_PLATFORM_CONFIG_INTERNAL_LUA_SACI_API_H
-#define SACI_PLATFORM_CONFIG_INTERNAL_LUA_SACI_API_H
-
-#include "saci_platform/config/lua.h"
-
-void psaci_lua_saci_api_init_tables(PSaciLuaState* lua);
-
-#endif // SACI_PLATFORM_CONFIG_INTERNAL_LUA_SACI_API_H
